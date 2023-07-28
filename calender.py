7aa7276be03a15add597283d82c6e6024ff11193
unknown <isishskw@gmail.com>
2023-07-28 15:36:15 +0900

    11th commit

--- calender.py
+++ calender.py
@@ -215,6 +215,123 @@ def generate_pdf():
     
     return df_comp
 
+def generate_pdf_noncol():
+
+    cols_nonex = ['Aパターン', 'Bパターン', '30日']
+    
+    #表が格子状になっている場合 lattice=True そうでない　stream=True　複数ページ読み込み pages='all'
+    df_calend = df[0]
+    #カラムを絞る
+    # df_calend = df_calend.drop('KX250AX\rKX260AX', axis=1)
+    
+    df_calend = df_calend.drop(df_calend.columns[[1, 5, 6, 7]], axis=1) #40日から右カラムの削除
+    df_calend = df_calend.dropna(how='any')
+    df_calend = df_calend.rename(columns={'Unnamed: 0': '受注日'})
+    df_calend = df_calend.loc[1:] #0行目受注日という文字列が入っている
+
+
+    #曜日を消す
+    for col in cols_nonex:
+        df_calend[col] = df_calend[col].str[:-2]
+    
+    #2022年の追加
+    for col in cols_nonex:
+        df_calend[col] = f'{this_year}年' + df_calend[col]
+    
+    #datetime型に変換
+    for col in cols_nonex:
+        df_calend[col] = pd.to_datetime(df_calend[col], format='%Y年%m月%d日')
+    
+    #年またぎ対応
+    #today datetime64型の用意　＊一度dfにしてから変換
+    today = datetime.date.today()
+    temp_list = []
+    temp_list.append(today)
+    df_temp = pd.DataFrame(temp_list, columns=['date'])
+    df_temp['date2'] = pd.to_datetime(df_temp['date'], format='%Y-%m-%d')
+    today2 = df_temp['date2'][0]
+
+    #todayより前のものは年を来年に変換
+    for col in cols_nonex:
+        df_calend[col] = df_calend[col].map(lambda x: x + relativedelta(years=1) if x < today2 else x)
+
+    #　str型へ　時間を消す
+    # Series型にはdtというアクセサが提供されており、Timestamp型を含む日時型の要素から、
+    # 日付や時刻のみの要素へ一括変換できる。
+    for col in cols_nonex:
+        df_calend[col] = df_calend[col].dt.strftime('%Y-%m-%d')
+    
+    #着日計算
+    arrival_a = []
+    arrival_b = []
+    arrival_30 = []
+
+    arrivals = [arrival_a, arrival_b, arrival_30]
+
+    i = 0
+    
+    for (col, a_list) in zip(cols_nonex, arrivals):
+        for shukka in df_calend[col]:
+            idx = kadoubi_2years.index(shukka) #list内の順番を検索抽出
+            arrival_culc = kadoubi_2years[idx + option_day] #着日算出
+
+            if arrival_culc in chakubi_2years:
+                a_list.append(arrival_culc)
+            else:
+                while arrival_culc not in chakubi_2years:
+                    i += 1
+                    arrival_culc = kadoubi_2years[idx + option_day + i] 
+                a_list.append(arrival_culc)
+                i = 0
+    
+    #書式変更　年を外して月日
+    arrival_a2 = []
+    arrival_b2 = []
+    arrival_302 = []
+
+    arraivals_change = [arrival_a, arrival_b, arrival_30]
+    arraivals2 = [arrival_a2, arrival_b2, arrival_302]
+    
+    for (a_list, a_list2) in zip(arraivals_change, arraivals2):
+        for chakubi in a_list:
+            chakubi = datetime.datetime.strptime (chakubi, '%Y-%m-%d')
+            chakubi = chakubi.strftime('%m/%d')
+            a_list2.append(chakubi)
+    
+    #df化
+    df_output = pd.DataFrame({
+        '受注日': df_calend['受注日'],
+        'A (レギュラー)': arrival_a2,
+        'B (下記参照)': arrival_b2,
+        'SEOTO-EX': arrival_b2,
+        'C（納期30日）': arrival_302,
+
+    })
+
+    #頭の0を無くする
+    a_list = []
+    b_list = []
+    ex_list = []
+    c_list = []
+
+    cols2 = ['A (レギュラー)', 'B (下記参照)', 'SEOTO-EX', 'C（納期30日）']
+    lists = [a_list, b_list, ex_list, c_list]
+
+    for (col, lst) in zip(cols2, lists):
+        for chakubi in df_output[col]:
+            split_c = chakubi.split('/')
+            month = split_c[0].lstrip("0")
+            day = split_c[1].lstrip("0")
+            c_without_0 =  month + "/" + day
+            lst.append(c_without_0)
+    
+    df_comp = pd.DataFrame(list(zip(df_output['受注日'], a_list, b_list, ex_list, c_list)), \
+                           columns=df_output.columns, index=df_output.index)
+    
+    return df_comp
+
+
+
 def generate_pdf_nonkxdate():
 
     cols_nonex = ['Aパターン', 'Bパターン', '30日']
@@ -383,19 +500,25 @@ if __name__ == '__main__':
     chakubi_2years = chakubi_this + chakubi_next 
 
     st.markdown('#### SEOTO-EX 日付あり/なし選択')
-    hizuke = st.selectbox('日付あり/なし選択', ['--', '日付なし/Ｂパターン', '日付あり'], key='hizuke')
+    hizuke = st.selectbox('日付あり/なし選択', ['--', '列名なし/Ｂパターン' ,'列名あり/日付なし/Ｂパターン', '列名あり/日付あり'], key='hizuke')
 
     if hizuke == '--':
         st.info('項目を選択してください')
         st.stop()
+    
+    elif hizuke== '列名なし/Ｂパターン':
+        df_comp = generate_pdf_noncol()
+        # to_excel(df_comp)
+        df_xlsx = to_excel(df_comp)
+        st.download_button(label='Download Excel file', data=df_xlsx, file_name= 'calender.xlsx')
 
-    elif hizuke== '日付なし/Ｂパターン':
+    elif hizuke== '列名あり/日付なし/Ｂパターン':
         df_comp = generate_pdf_nonkxdate()
         # to_excel(df_comp)
         df_xlsx = to_excel(df_comp)
         st.download_button(label='Download Excel file', data=df_xlsx, file_name= 'calender.xlsx')
 
-    elif hizuke == '日付あり':
+    elif hizuke == '列名あり/日付あり':
         df_comp = generate_pdf()
         # to_excel(df_comp)
         df_xlsx = to_excel(df_comp)
